49751d00a06c1a26e82a859f902aae01feafc642
PabloBarTo <pablo02barto@correo.ugr.es>
2024-03-06 16:52:04 +0100

    Resolución del tutorial

--- Comportamientos_Jugador/jugador.cpp
+++ Comportamientos_Jugador/jugador.cpp
@@ -7,6 +7,8 @@ Action ComportamientoJugador::think(Sensores sensores)
 
 	Action accion = actIDLE;
 
+	int a;
+
 	// Mostrar el valor de los sensores
 	cout << "Posicion: fila " << sensores.posF << " columna " << sensores.posC;
 	switch (sensores.sentido)
@@ -32,10 +34,89 @@ Action ComportamientoJugador::think(Sensores sensores)
 	cout << "  Reset: " << sensores.reset;
 	cout << "  Vida: " << sensores.vida << endl<< endl;
 
+	switch(last_action){
+		case actWALK:
+			switch(current_state.brujula){
+				case norte:
+					current_state.fil--;
+					break;
+				case noreste:
+					current_state.fil--;
+					current_state.col++;
+					break;
+				case este:
+					current_state.col++;
+					break;
+				case sureste:
+					current_state.fil++;
+					current_state.col++;
+					break;
+				case sur:
+					current_state.fil++;
+					break;
+				case suroeste:
+					current_state.fil++;
+					current_state.col--;
+					break;
+				case oeste:
+					current_state.col--;
+					break;
+				case noroeste:
+					current_state.fil--;
+					current_state.col--;
+					break;
+			}
+			break;
+
+		case actRUN:
+			
+			break;
+
+		case actTURN_SR:
+			a = current_state.brujula;
+			a = (a + 1) % 8;
+			current_state.brujula = static_cast<Orientacion>(a);
+			break;
+		case actTURN_L:
+			a = current_state.brujula;
+			a = (a + 6) % 8;
+			current_state.brujula = static_cast<Orientacion>(a);
+			break;
+	}
+
+	if(sensores.terreno[0]=='G' and !bien_situado){
+		current_state.fil = sensores.posF;
+		current_state.col = sensores.posC;
+		current_state.brujula = sensores.sentido;
+		bien_situado = true;
+	}
+
+	if(bien_situado){
+		PonerTerrenoEnMatriz(sensores.terreno, current_state, mapaResultado);
+	}
+
+	if((sensores.terreno[2]=='T' or sensores.terreno[2]=='S' or sensores.terreno[2]=='G') and sensores.agentes[2]=='_'){
+		accion = actWALK;
+	}
+	else if (!girar_derecha){
+		accion = actTURN_L;
+		girar_derecha = (rand()%2==0);
+	}
+	else{
+		accion = actTURN_SR;
+		girar_derecha = (rand()%2==0);
+	
+	}
+
+	last_action = accion;
 	return accion;
 }
 
 int ComportamientoJugador::interact(Action accion, int valor)
 {
 	return false;
+}
+
+void ComportamientoJugador::PonerTerrenoEnMatriz(const vector<unsigned char> &terreno, const state &st, vector<vector<unsigned char>> &matriz){
+	matriz[st.fil][st.col] = terreno[0];
 }
--- Comportamientos_Jugador/jugador.hpp
+++ Comportamientos_Jugador/jugador.hpp
@@ -4,12 +4,25 @@
 #include "comportamientos/comportamiento.hpp"
 using namespace std;
 
+struct state{
+  int fil; 
+  int col;
+  Orientacion brujula;
+};
+
 class ComportamientoJugador : public Comportamiento{
 
   public:
     ComportamientoJugador(unsigned int size) : Comportamiento(size){
       // Constructor de la clase
       // Dar el valor inicial a las variables de estado
+      current_state.fil = 99;
+      current_state.col = 99;
+      current_state.brujula = norte;
+      last_action = actIDLE;
+      girar_derecha = false;
+      bien_situado = false;
+
     }
 
     ComportamientoJugador(const ComportamientoJugador & comport) : Comportamiento(comport){}
@@ -17,8 +30,13 @@ class ComportamientoJugador : public Comportamiento{
 
     Action think(Sensores sensores);
     int interact(Action accion, int valor);
+    void PonerTerrenoEnMatriz(const vector<unsigned char> &terreno, const state &st, vector<vector<unsigned char>> &matriz);
 
   private:
   // Declarar aquí las variables de estado
+    state current_state;
+    Action last_action;
+    bool girar_derecha;
+    bool bien_situado;
 };
 #endif
